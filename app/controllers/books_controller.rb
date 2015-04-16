74475f4d6c1cf9bd885d36027a759baeaa9fdde4
hieu <hieu@ubuntu.(none)>
2015-04-16 17:09:48 +0700

    search book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,11 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.paginate page: params[:page], per_page: 10
+    if params[:type].present?
+      @books = Book.find_book params[:search], params[:type]
+    else
+      @books = Book.all
+    end
+    @books = @books.paginate page: params[:page], per_page: 10
   end
 
   def show
--- app/models/book.rb
+++ app/models/book.rb
@@ -6,4 +6,23 @@ class Book < ActiveRecord::Base
   has_many :reviews
 
   scope :books, -> book_ids {where(id: book_ids)}
+  scope :find_name, -> search {where("name LIKE '%#{search}%'")}
+  scope :find_title, -> search {where("title LIKE '%#{search}%'")}
+  scope :find_category, -> search {where("category_id LIKE '%#{search}%'")}
+  scope :find_author, -> search {where("author LIKE '%#{search}%'")}
+
+  def self.find_book search, type
+    case type
+    when '1' 
+      @books = Book.find_name search
+    when '2' 
+      @books = Book.find_title search
+    when '3' 
+      @books = Book.find_category search
+    when '4' 
+      @books = Book.find_author search
+    else 
+      @books = Book.all
+    end
+  end
 end
